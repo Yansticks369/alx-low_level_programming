951da01d3247408a5d2b0edd696b21f03c0d946b
Yansticks369 <anyasocharles.ace28@gmail.com>
2022-09-08 16:08:13 -0700

    low level

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Short description, single line
+ * Description: Longer description of the fuction)?
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
