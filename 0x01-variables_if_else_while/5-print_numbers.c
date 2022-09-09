35f3a5e10d6128029710eacda1c7bbe8ee43b716
Yansticks369 <111122541+Yansticks369@users.noreply.github.com>
2022-09-10 00:49:47 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,12 @@
 */
 int main(void)
 {
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("%i", i);
-  }
-  printf("\n");
-  return (0);
+int i;
+  
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+printf("\n");
+return (0);
 }
