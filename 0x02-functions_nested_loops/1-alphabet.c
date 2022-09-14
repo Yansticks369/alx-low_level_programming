4fae68c0630effc05a24880113833bc752880728
Yansticks369 <anyasocharles.ace28@gmail.com>
2022-09-13 17:28:35 -0700

    second commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -13,34 +13,4 @@ void print_alphabet(void)
 		_putchar(c);
 	}
 	_putchar('\n');
-}#include "main.h"
-/**
- * print_alphabet - function to print abc
- *
- * Return: 0
- */
-void print_alphabet(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
-}#include "main.h"
-/**
- * print_alphabet - function to print abc
- *
- * Return: 0
- */
-void print_alphabet(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
 }
