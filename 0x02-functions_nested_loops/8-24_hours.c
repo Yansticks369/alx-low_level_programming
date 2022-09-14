f2beab574a5f6443bc2db57cf71162e2ce367889
Yansticks369 <anyasocharles.ace28@gmail.com>
2022-09-13 20:25:41 -0700

    my commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * jack_bauer - func that prints every minute of the day of Jack Bauer, starting f
- * rom 00:00 to 23 z;59, min loop counts mins, while hour loop counts hours and re
- * sets mins
+ * jack_bauer - func that prints every minute of the day of Jack Bauer, starti
+ * n from 00:00 to 23 z;59, min loop counts mins, while hour loop counts hours
+ * and resets mins
  * Return: 0
  */
 void jack_bauer(void)
