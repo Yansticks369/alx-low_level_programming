298ecebc7909d1e1c478d92942491d2ad516d6c1
Yansticks369 <111122541+Yansticks369@users.noreply.github.com>
2022-09-16 06:45:52 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,17 +16,17 @@ void print_triangle(int size)
 		{
 			for ((inc2 = size - inc1); inc2 > 0; inc2--)
 			{
-				putchar(' ');
+				_putchar(' ');
 			}
 			for (inc2 = 0; inc2 < inc1; inc2++)
 			{
-				putchar('#');
+				_putchar('#');
 			}
 			if (inc1 == size)
 			{
 				continue;
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
