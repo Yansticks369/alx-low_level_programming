396d64cc4eeed0075306277e8750d649fa1a1d15
Yansticks369 <anyasocharles.ace28@gmail.com>
2022-09-15 22:19:39 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,10 +15,11 @@ void print_square(int size)
 	{
 		for (inc1 = 0; inc1 < size; inc1++)
 		{
-			for (inc2 = 0; in2 < (size - 1); inc2++)
+			for (inc2 = 0; inc2 < (size - 1); inc2++)
 			{
 				putchar('#');
 			}
+
 			putchar('#');
 			putchar('\n');
 		}
