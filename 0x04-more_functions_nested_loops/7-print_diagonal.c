02f0a33ecb0b61f2ab69e5bc91d2bfeac556826d
Yansticks369 <111122541+Yansticks369@users.noreply.github.com>
2022-09-16 02:48:58 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,17 +17,17 @@ void print_diagonal(int n)
 		{
 			for (space = 0; space < len; space++)
 			{
-				_putchar(' ');
+				putchar(' ');
 			}
 
-			_putchar('\\');
+			putchar('\\');
 
 			if (len == (n - 1))
 			{
 				continue;
 			}
-			_putchar('\n');
+			putchar('\n');
 		}
 	}
-	_putchar('\n');
+	putchar('\n');
 }
