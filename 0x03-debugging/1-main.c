eb1a2c355e0773a40b166a9d9e40f270f6c52cf0
Yansticks369 <anyasocharles.ace28@gmail.com>
2022-09-16 07:09:08 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,10 +8,11 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("infinite loop incoming :(\n");
-	
+
 	i = 0;
+
 	/*
 	 * while (i < 10)
 	 * {
@@ -19,6 +20,5 @@ int main(void)
 	 * }
 	 */
 	printf("infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
