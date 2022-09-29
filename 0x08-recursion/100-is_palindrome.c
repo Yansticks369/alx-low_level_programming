d8602a3da4c2fd90d5177b7fa074b85711a54488
Yansticks369 <111122541+Yansticks369@users.noreply.github.com>
2022-09-29 02:13:22 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,54 +1,47 @@
 #include "main.h"
 
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
 /**
- * is_palindrome - returns the l if is a palindrome
- * @s: string to be checked
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
  *
- * Return: 1 if s is a palindrome, 0 otherwise
+ * Return: 1 if it is, 0 it's not
  */
 int is_palindrome(char *s)
 {
-	int flag = 1;
-
-	check(s, 0, _strlen_recursion(s) - 1, &flag);
-	return (flag);
+ if (*s == 0)
+  return (1);
+ return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
- * check - checks if a string is a palindrome
- * @s: string to be checked
- * @start: start index
- * @end: end index
- * flag: flag to indicate if a string is a palindrome
- *
- * Return: void
- */
-void check(char *s, int start, int end, int *flag)
-{
-	if (start <= end)
-	{
-		if (s[start] == s[end])
-			*flag *= 1;
-		else
-			*flag *= 0;
-		check(s, start + 1, end - 1, flag);
-	}
-}
-
-/**
- * _strlen_recursion - calculates the length of a string
- * @s: string to be used
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
  *
  * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
-	int sum = 0;
+ if (*s == '\0')
+  return (0);
+ return (1 + _strlen_recursion(s + 1));
+}
 
-	if (*s != '\0')
-	{
-		sum++;
-		sum += _strlen_recursion(s + 1);
-	}
-	return (sum);
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+ if (*(s + i) != *(s + len - 1))
+  return (0);
+ if (i >= len)
+  return (1);
+ return (check_pal(s, i + 1, len - 1));
 }
