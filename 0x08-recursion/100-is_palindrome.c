fc2a579f930829c9d3760b90117f4e48cfff5937
Yansticks369 <111122541+Yansticks369@users.noreply.github.com>
2022-09-29 02:07:59 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -23,7 +23,7 @@ int is_palindrome(char *s)
  *
  * Return: void
  */
-void check(char *s, int start, int end, int *flag)
+void check(char *s, int start, int end, int flag)
 {
 	if (start <= end)
 	{
