62fdad463bfdcd777cb9eb7742b5712005e00c19
Yansticks369 <anyasocharles.ace28@gmail.com>
2022-10-03 06:19:16 -0700

    task 1

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,17 +9,17 @@ int len(char *str)
 {
 	int len = 0;
 
-		if (str != NULL)
-		{
-			while (str[len])
-				len++;
-		}
+	if (str != NULL)
+	{
+		while (str[len])
+			len++;
+	}
 	return (len);
 }
 
 /**
  * argstostr - a function that concatenates all the arguments of your program
- * ac: count of args passed to the function
+ * @ac: count of args passed to the function
  * @av: array of arguments
  *
  * Return: pointer to the new string
