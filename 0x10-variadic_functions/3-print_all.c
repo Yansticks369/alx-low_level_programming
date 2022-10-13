d8c5b99afa9ad699cab77e9df95d4f6db277d489
Yansticks369 <111122541+Yansticks369@users.noreply.github.com>
2022-10-13 23:50:56 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,12 +2,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-void print_all(const char * const format, ...);
-
 /**
  * print_char - Prints a char.
  * @arg: A list of arguments pointing to
